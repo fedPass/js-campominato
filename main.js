9e208566ff068fa9ac1bf282b24ed5ef425826a1
fedPass <federica.passanante@gmail.com>
2019-11-22 17:59:03 +0100

    bonus

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
+//richiedere difficoltà all’utente difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50
+var numeroGiocate = 0;
+
+var livelloDifficolta = prompt('Seleziona livello tra Facile (digita 0) / Medio (digita 1) / Difficile (digita 2)');
+
+// con Switch
+switch (livelloDifficolta) {
+    case 0:
+        numeroGiocate =  100;
+        break;
+    case 1:
+        numeroGiocate =  80;
+        break;
+    case 2:
+        numeroGiocate =  50;
+        break;
+    default:
+        console.log('Hai scelto il livello ' + livelloDifficolta);
+}
+
+// con multi if
+if (livelloDifficolta == 0) {
+    numeroGiocate =  100;
+} else if (livelloDifficolta == 1) {
+    numeroGiocate =  80;
+} else if (livelloDifficolta == 2) {
+    numeroGiocate =  50;
+} else {
+    console.log('non hai inserito un numero valido');
+}
+
 // A. generare 16 numeri casuali tra 1 e 100 (queste saranno le mine).
 // A1.creo l’array(vuoto)
  var mine = [];
  while (mine.length < 16) {
     // A2. Creo un numero causale
-    var numero = generaRandom(1,100);
+    var numero = generaRandom(1,numeroGiocate);
     console.log(numero);
     // A3. Controllo che il numero non ci sia nell’Array
     if(!mine.includes(numero)) {
@@ -50,8 +81,8 @@ function generaRandom(min, max) {
 }
 
 //funzione che controlla che il numeroUtente è valido (è numero, compreso tra 1 e 100, non inserito precedentemente)
-function numeroNonValido (numeroUtente, numeriInseriti) {
-    if (isNaN(numeroUtente) && numeroUtente < 1 && numeroUtente > 100 && numeriInseriti.includes(numeroUtente)) {
+function numeroNonValido(numeroUtente, numeriInseriti) {
+    if (isNaN(numeroUtente) && (numeroUtente < 1) && (numeroUtente > 100) && numeriInseriti.includes(numeroUtente)) {
         return true;
     } else {
         return false;
