d9e11d6ade88c9e7a00ac6b2c2a22103c5fc0d03
fedPass <federica.passanante@gmail.com>
2019-11-22 16:10:16 +0100

    funzione per verificare numero utente

--- main.js
+++ main.js
@@ -9,47 +9,51 @@
     if(!mine.includes(numero)) {
         // A4. Se non c’è lo metto nell’array
         mine.push(numero);
-        console.log(mine);
     }
 }
+console.log('Mine generate: ' + mine);
+
+// B. chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// chiedi un numero finchè numeroUtente non è incluso in mine
+var numeroTentativi = 0;
+var numeriInseriti = [];
+
+do {
+    //chiedo un numero tra 1 e 100
+    var numeroUtente = parseInt(prompt('Inserisci un numero tra 1 e 100'));
+    console.log('Hai inserito il numero ' + numeroUtente);
+
+    if (numeroNonValido(numeroUtente, numeriInseriti)) {
+        console.log('non hai inserito un numero valido');
+    } else {
+        numeroTentativi++;
+        numeriInseriti.push(numeroUtente);
+    }
+} while (!mine.includes(numeroUtente) && numeroTentativi < 84);
+// La partita termina quando il giocatore inserisce un numero “mina” o raggiunge il numero massimo possibile di numeri consentiti
+// inserisce un numero “mina” --> !mine.includes(numeroUtente)
+// raggiunge il numero massimo --> (numeroTentativi < 84)
+
+
+if (numeroTentativi == 84) {
+    console.log('hai vinto..che culo!');
+} else {
+    console.log('Boom! Hai preso una mina');
+    //C. Al termine della partita il software deve comunicare il numero di volte che l’utente ha inserito un numero consentito.
+    console.log('Hai perso dopo ' + numeroTentativi + ' tentativi');
+}
+
 // funzione che genera un numero random tra min e max
 function generaRandom(min, max) {
     var random = Math.floor(Math.random() * (max - min + 1) ) + min;
     return random;
 }
-// B. chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// chiedi un numero finchè numeroUtente non è incluso in mine
-var numeroTentativi = 0;
-do {
-    var numeroUtente = parseInt(prompt('Inserisci un numero tra 1 e 100'));
-    console.log('Hai inserito il numero ' + numeroUtente);
-    if ((numeroUtente > 1) && (numeroUtente > 100)) {
-        console.log('non hai inserito un numero valido');
-    }
-    numeroTentativi++;
-} while ((!mine.includes(numeroUtente) && (numeroTentativi < 84)));
-// La partita termina quando il giocatore inserisce un numero “mina” o raggiunge il numero massimo possibile di numeri consentiti
-// inserisce un numero “mina” --> !mine.includes(numeroUtente)
-// raggiunge il numero massimo --> (numeroTentativi < 84)
-console.log('Boom! Hai preso una mina');
-//C. Al termine della partita il software deve comunicare il numero di volte che l’utente ha inserito un numero consentito.
-console.log('Hai perso dopo ' + numeroTentativi + ' tentativi');
 
-// Prova precedente
-// var numeroUtente = parseInt(prompt('Inserisci un numero tra 1 e 100'));
-// if ((numeroUtente > 1) && (numeroUtente > 100)) {
-//     console.log('non hai inserito un numero valido');
-// } else {
-//     console.log('hai inserito ' + numeroUtente);
-//     for (var i = 0; i < mine.length; i++) {
-//         // C. Se il numero è presente nella lista dei numeri generati, la partita termina (l'utente ha beccato una mina)
-//         if (numeroUtente == mine[i]) {
-//             console.log('Boom! Hai preso una mina');
-//         } else {
-//         // altrimenti si continua chiedendo all’utente un altro numero.
-//         //come faccio ripartire?
-//         //mi serve una funzione forse ???
-//             console.log('Inserisci un altro numero');
-//         }
-//     }
-// }
+//funzione che controlla che il numeroUtente è valido (è numero, compreso tra 1 e 100, non inserito precedentemente)
+function numeroNonValido (numeroUtente, numeriInseriti) {
+    if (isNaN(numeroUtente) && numeroUtente < 1 && numeroUtente > 100 && numeriInseriti.includes(numeroUtente)) {
+        return true;
+    } else {
+        return false;
+    }
+}
